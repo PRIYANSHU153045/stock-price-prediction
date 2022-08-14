687bcf6de5dc45ce51afe68e491ae060f101abc2
Priyanshu <priyanshusingh153045@gmail.com>
2022-08-14 19:47:09 +0530

    third commit

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
-import yfinance as fn
-st.write(""" My first App  """)
-st.title("Stock Market App with streamlit")
-st.header("Simple data science web app")
-st.sidebar.header("Bek Brace \n Code along with me...")
-# This is a function that fetches the company's ticker 
-def get_ticker(name):
-	company=fn.Ticker(name)
-	return company
+from datetime import date
 
-c1=get_ticker("AAPL")
-c2=get_ticker("MSFT")
-c3=get_ticker("TSLA")
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
 
-apple=fn.download("AAPL",start="2021-11-11",end="2022-11-11")
-microsoft=fn.download("MSFT",start="2021-11-11",end="2022-11-11")
-tesla=fn.download("TSLA",start="2021-11-11",end="2022-11-11")
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-data1=c1.history(period="3mo")
-data2=c2.history(period="3mo")
-data3=c3.history(period="3mo")
+st.title('Stock Forecast App')
 
-st.write("""## Apple""")
-st.write(c1.info['longBusinessSummary'])
-st.write(apple)
-st.line_chart(data1.values)
-st.write("""## Microsoft""")
-st.write(c2.info['longBusinessSummary'])
-st.write(microsoft)
-st.line_chart(data2.values)
-st.write("""## Tesla""")
-st.write(c3.info['longBusinessSummary'])
-st.write(tesla)
-st.line_chart(data3.values)
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
